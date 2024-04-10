2963bd5fe02a85ac36da5328d9d778cdbf8267e4
U-OZ <166579824+U-OZ@users.noreply.github.com>
2024-04-10 13:20:05 +0900

    Update variant1.py

--- variant1.py
+++ variant1.py
@@ -1,5 +1,10 @@
+#かけ算表　12*12
+
+#xはかける数
 for x in range(1, 13):
+    #yはかけられる数
     for y in range(1, 13):
         ans = x * y
+        #出力される値を等間隔で表示
         print(f'{ans:<6}', end="")
     print()
